fde99aae7a5e05f76c82b7c957d2cadbde692c8d
JeongYoon <81298238+jeongyoon05@users.noreply.github.com>
2023-02-10 00:25:27 +0900

    chore: (#1) project setting (#4)

---
+++ src/test/kotlin/github/yee/fullhousebackend/FullHouseBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.yee.fullhousebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FullHouseBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
