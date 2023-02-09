4110a439b2264f31339fdb9d39e5c62ca2d0561a
Jeong Yoon <dlwjddbs14@naver.com>
2023-02-10 00:12:18 +0900

    chore: (#1) project setting

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Full-House-Backend"
---
+++ src/main/kotlin/github/yee/fullhousebackend/FullHouseBackendApplication.kt
@@ -0,0 +1,11 @@
+package github.yee.fullhousebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FullHouseBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FullHouseBackendApplication>(*args)
+}
